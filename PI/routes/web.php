ca1eeda279a3429ef5cc9b348cb74fe40eb24b01
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2024-11-21 18:20:54 -0600

    node

--- PI/routes/web.php
+++ PI/routes/web.php
@@ -20,4 +20,5 @@ Route::get('/usuarios',[ControladorVistas::class,'usuario'])->name('rutaUsuarios
 //Ruta Reportes
 Route::get('/reporte/create',[reporteController::class,'create'])->name('rutaReportes');
 // sesion
-Route::post('/sesion',[ControladorVistas::class,'ValidadorSesion'])->name('rutaReportes');
+Route::post('/sesion',[ControladorVistas::class,'ValidadorSesion'])->name('rutaReportes'); 
+
